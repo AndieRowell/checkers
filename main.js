8a6a8a2c70bafc678941b4fbec9c922c97790d0b
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-03 13:51:11 -0400

    adding pseudocode

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-// main.js
+//* FUNCTIONAL PSEUDOCODE
 
-function init(){
-    console.log("hello world!");
+
+document.addEventListener("load", init); //when the page is being loaded call the init function?
+let gameState = ();
+
+init(){
+    // create the game object
+    game = newGame();
+    // create the player1 and player2 objects
+    player1 = new Player();
+    player2 = new Player();
+    // update or populate gameState
+    // add properties to the gameState with values
+    // build UI
+    buildUI();
 }
+
+function newGame(){
+    // create game object
+    // check local storage
+    // if we have a game in local storage
+    // hydrate state
+}
+
+
+
+// look up model view controller? MVC
+// review object oriented programming
